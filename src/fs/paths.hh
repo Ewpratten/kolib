0417bc6dd3c57e90f8e4841529e94a0f448a1922
Evan Pratten <ewpratten@gmail.com>
2021-01-08 15:44:51 -0500

    more builds

---
+++ src/fs/paths.hh
@@ -0,0 +1,15 @@
+#if !defined(_KOLIB_FS_PATHS)
+#define _KOLIB_FS_PATHS
+
+#include <string>
+#include <memory>
+
+namespace kobo {
+namespace fs {
+
+std::shared_ptr<std::string> getOnboardStorageLocation();
+
+}  // namespace fs
+}  // namespace kobo
+
+#endif  // _KOLIB_FS_PATHS
